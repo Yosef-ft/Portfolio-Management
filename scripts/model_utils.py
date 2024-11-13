0b89f03a27b26c4c12d319fe81818d2d758cfc3e
Yosef-ft <yosefetene02@gmail.com>
2024-11-13 16:12:40 +0300

    Updated script for training and predict LSTM model

--- scripts/model_utils.py
+++ scripts/model_utils.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+from datetime import datetime, timedelta
 
 import pandas as pd
 import numpy as np 
@@ -10,13 +11,17 @@ import yfinance as yf
 import sidetable as stb
 import mplfinance as mpf
 from scipy.stats import zscore
+import joblib
+import pickle
 
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.graphics.tsaplots import plot_pacf
 from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.tsa.stattools import adfuller
 
+import plotly.graph_objects as go
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
 
 from tensorflow.keras.losses import MeanSquaredError
 from tensorflow.keras.metrics import MeanSquaredError, RootMeanSquaredError, MeanAbsoluteError
@@ -177,53 +182,124 @@ class ModelUtil:
         print(f"Mean Absolute Percentage Error (MAPE): {mape}%")
 
 
-    def create_sequence(self, data, time_steps =60):
+    def create_sequence(self, data, time_steps =63):
         X, y = [], []
         for i in range(len(data) - time_steps):
             X.append(data[i:(i + time_steps)])
             y.append(data[i + time_steps])
         return np.array(X), np.array(y)
 
-    def train_lstm(self, data, seq_length=60, epochs=10, batch_size=32 ,model_name='LSTM'):
-        column = []
-        train, test, train_dates, test_dates, series = self.train_test_split(data=data, test_size= seq_length, model_name= model_name)
+    def train_lstm(self, data, seq_length, model_name, ticker, epochs=10, batch_size=32):
 
-        data = train[['Close', 'ATR', 'Bhband', 'Bhband_indicator', 'Bma', 'Dhband', 'Dlband',
-        'Mband', 'ketler']].values.reshape(-1, 1)
-        
-        data2 = test[['Close', 'ATR', 'Bhband', 'Bhband_indicator', 'Bma', 'Dhband', 'Dlband',
-        'Mband', 'ketler']].values.reshape(-1, 1)
-        # data = train[['Close', 'ATR', 'Bhband', 'Bhband_indicator', 'Bma', 'Dhband', 'Dlband', 'Mband', 'ketler']].values
-        # data2 = test[['Close', 'ATR', 'Bhband', 'Bhband_indicator', 'Bma', 'Dhband', 'Dlband', 'Mband', 'ketler']].values    
+        start_time = time.time()
+        logger.info(f"Training LSTM model for {ticker}...")
 
-        # Create sequences
-        X_train, y_train = self.create_sequence(data, seq_length)
-        X_val, y_val = self.create_sequence(data2, seq_length)
+        train, test, train_dates, test_dates, series = self.train_test_split(data=data, test_size=seq_length, model_name=model_name)
+
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        train_scaled = scaler.fit_transform(train['Close'].values.reshape(-1, 1))
+        test_scaled = scaler.transform(test['Close'].values.reshape(-1, 1))
+
+        joblib.dump(scaler, f'../Models/{ticker}-scaler.joblib')
+
+        X_train, y_train = self.create_sequence(train_scaled, seq_length)
+        X_val, y_val = self.create_sequence(test_scaled, seq_length)
+
+        X_train = X_train.reshape(X_train.shape[0], seq_length, train_scaled.shape[1])
+        X_val = X_val.reshape(X_val.shape[0], seq_length, train_scaled.shape[1])
 
         model = Sequential()
-        model.add(Input(shape=(seq_length, 1)))
+        model.add(Input(shape=(seq_length, train_scaled.shape[1])))
         model.add(LSTM(50, activation='relu', return_sequences=True))
-        model.add(Dropout(0.2))
+        model.add(Dropout(0.1))
         model.add(LSTM(50, activation='relu'))
-        model.add(Dropout(0.2))
+        model.add(Dropout(0.1))
         model.add(Dense(1))
 
         model.compile(optimizer='adam', loss='mse')
-        # model.compile(
-        #     loss=MeanSquaredError(),
-        #     optimizer=Adam(learning_rate=0.01),
-        # )    
-        model.summary()  
+        model.summary()
 
         callback = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 
         history = model.fit(
-            X_train, y_train, epochs=epochs, batch_size=batch_size,validation_data=(X_val, y_val),
-            callbacks=[callback], verbose=0
+            X_train, y_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(X_val, y_val),
+            callbacks=[callback],
+            verbose=0
         )
 
-        return model, history        
-    
+        folder_path = '../Models/'
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        
+        timestamp = datetime.now().strftime("%d-%m-%Y-%H-%M-%S-00")
+        filename = f'{folder_path}{ticker}-{timestamp}.pkl'
+        
+        with open(filename, 'wb') as file:
+            pickle.dump(model, file)
+
+        print(f"Model saved as {filename}")
+
+        end_time = time.time()
+        logger.info(f"Model training took {round(end_time - start_time,2)} seconds")
+
+        return model, history
+
+    def lstm_predict_future(self, data, ticker, model_path, scaler_path, predict_days):
+
+        with open(model_path, 'rb') as file:
+            model = pickle.load(file)
+        scaler = joblib.load(scaler_path)
+
+        time_step = 252 * 4  # Last 4 years 252 trading days 
+        last_data = data[-time_step:]  
+        last_data_scaled = scaler.transform(last_data['Close'].values.reshape(-1, 1))  
+        
+        input_seq = last_data_scaled.reshape(1, time_step, last_data_scaled.shape[1])
+        predictions = []
+        current_date = pd.to_datetime(data.index[-1]) + timedelta(days=1)  
+
+        for _ in range(predict_days):
+            predicted_price_scaled = model.predict(input_seq, verbose=0)
+            
+            predicted_price = scaler.inverse_transform(predicted_price_scaled[0][0].reshape(-1,1))
+            
+            predictions.append((current_date, predicted_price))
+            
+            input_seq = np.append(input_seq[:, 1:, :], [[predicted_price_scaled[0]]], axis=1)
+            
+            current_date += timedelta(days=1)
+
+        prediction_df = pd.DataFrame(predictions, columns=['Date', f'{ticker}'])
+        prediction_df.set_index('Date', inplace=True)
+        
+        return prediction_df
+
+
+    def plot_forecast(self, ticker, historical_data, forecasted_data, confidence_interval):
+        conf_int_pct = (1 - confidence_interval) * 100
+        
+        lower_bound = forecasted_data[ticker] * (1 - confidence_interval)
+        upper_bound = forecasted_data[ticker] * (1 + confidence_interval)
+        
+        fig = go.Figure()
+        
+        fig.add_trace(go.Scatter(x=historical_data.index, y=historical_data[ticker], mode='lines', name=f'{ticker} Historical Data', line=dict(color='blue')))
+        fig.add_trace(go.Scatter(x=forecasted_data.index, y=forecasted_data[ticker], mode='lines', name=f'{ticker} Forecasted Price', line=dict(color='orange')))
+        fig.add_trace(go.Scatter(x=forecasted_data.index, y=upper_bound, mode='lines', fill=None, line=dict(color='gray'), showlegend=False))
+        fig.add_trace(go.Scatter(x=forecasted_data.index, y=lower_bound, fill='tonexty',  line=dict(color='gray'), name='Confidence Interval'))
+        
+        fig.update_layout(title=f"{ticker} Stock Price Forecast with {conf_int_pct:.2f}% Confidence Interval",
+                        xaxis_title="Date",
+                        yaxis_title="Price",
+                        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
+                        margin=dict(l=50, r=50, t=50, b=50),
+                        template='plotly_white')
+        
+        fig.show()
+
     def model_metrics_Lstm(self, model, model_name, data, test_size):
         train, test, train_dates, test_dates, series = self.train_test_split(data, test_size, model_name)
 
